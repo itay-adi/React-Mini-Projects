1ed47a42b44bb5b8fc9b3ab7e76ea32bd3b43406
itay-adi <itayzxcv@gmail.com>
2021-08-18 16:55:11 +0300

    Class Based

--- Class Based/src/components/User.js
+++ Class Based/src/components/User.js
@@ -3,6 +3,10 @@ import classes from './User.module.css';
 import { Component } from 'react'; 
 
 class User extends Component{
+  componentWillUnmount(){
+    console.log('User will unmouunt');
+  }
+
   render(){
     return <li className={classes.user}>{this.props.name}</li>;
   }
