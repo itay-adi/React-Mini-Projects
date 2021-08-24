ee4a47712a11a9028809a3b3c745059fd737961d
itay-adi <itayzxcv@gmail.com>
2021-08-24 13:35:34 +0300

    redux toolkit

--- Redux_react/src/store/index.js
+++ Redux_react/src/store/index.js
@@ -1,13 +1,35 @@
 import { createStore } from 'redux';
 
+const initialState = { counter: 0, showCounter: true }; 
+
 //Here we create the reducer function, params are defalut
-const counterReducer = (state = { counter: 0}, action) => {
+const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment'){
-        return ({counter: state.counter + 1});
+        return ({
+            counter: state.counter + 1,
+            showCounter: state.showCounter
+        });
+    }
+
+    if(action.type === 'increase'){
+        return ({
+            counter: state.counter + action.amount,
+            showCounter: state.showCounter
+        });
     }
 
     if(action.type === 'decrement'){
-        return ({counter: state.counter - 1});
+        return ({
+            counter: state.counter - 1,
+            showCounter: state.showCounter
+        });
+    }
+
+    if(action.type === 'toggle'){
+        return ({
+            counter: state.counter,
+            showCounter: !state.showCounter
+        });
     }
 
     return state;
