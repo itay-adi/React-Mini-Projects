5cfa0ab2460ca79fe270eab99de75afcc429df2a
itay-adi <itayzxcv@gmail.com>
2021-08-25 13:03:41 +0300

    Basic Redux

--- Redux_react/src/store/index.js
+++ Redux_react/src/store/index.js
@@ -1,41 +1,62 @@
-import { createStore } from 'redux';
+//import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-const initialState = { counter: 0, showCounter: true }; 
+import counterReducer from './counter';
+import authReducer from './auth'; 
+
+/****************************************************************/
+
+//Here we create a store. We create only one!
+const store = configureStore({
+    //reducer here is a map of reducers
+    reducer: { 
+        counter: counterReducer,
+        auth: authReducer
+    }
+});
+
+export default store;
+
+/****************************************************************/
 
 //Here we create the reducer function, params are defalut
-const counterReducer = (state = initialState, action) => {
+/*const counterReducer = (state = initialState, action) => {
     if(action.type === 'increment'){
         return ({
+            ...state,
             counter: state.counter + 1,
-            showCounter: state.showCounter
+            //showCounter: state.showCounter
         });
     }
 
     if(action.type === 'increase'){
         return ({
+            ...state,
             counter: state.counter + action.amount,
-            showCounter: state.showCounter
+            //showCounter: state.showCounter
         });
     }
 
     if(action.type === 'decrement'){
         return ({
+            ...state,
             counter: state.counter - 1,
-            showCounter: state.showCounter
+            //showCounter: state.showCounter
         });
     }
 
     if(action.type === 'toggle'){
         return ({
-            counter: state.counter,
+            ...state,
+            //counter: state.counter,
             showCounter: !state.showCounter
         });
     }
 
     return state;
-}
+}*/
 
 //Here we create a store
-const store = createStore(counterReducer);
+/*const store = createStore(counterReducer);
 
-export default store;
+export default store;*/
