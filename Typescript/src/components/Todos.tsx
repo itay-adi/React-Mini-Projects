05d1616d0ea3018278c126457bf5079edffebc7c
itay-adi <itayzxcv@gmail.com>
2021-08-31 10:53:50 +0300

    TS

--- Typescript/src/components/Todos.tsx
+++ Typescript/src/components/Todos.tsx
@@ -1,9 +1,14 @@
-function Todos(){
+import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
+import classes from './Todos.module.css';
+
+const Todos: React.FC<{items: Todo[]; onRemoveTodo: (id: string) => void}> = (props) => {
     return (
-        <ul>
-            <li>Learn React</li>
-            <li>Learn TS</li>
-        </ul>
+        <ul className={classes.todos}>
+            {props.items.map(item => (
+                <TodoItem key={item.id} text={item.text} onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}/>
+            ))}
+        </ul> 
     );
 }
 
